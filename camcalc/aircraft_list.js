c35bd5734358e3abe65913474381dde2ba029b75
jcofdi <66176356+jcofdi@users.noreply.github.com>
2026-03-18 10:10:37 -0500

    Add files via upload

---
+++ camcalc/aircraft_list.js
@@ -0,0 +1,64 @@
+// ============================================================================
+//  DCS Aircraft List for CamCalc
+//  Format:  ["Display Name", "DCS Filename"]
+//
+//  Display Name  = what you see in the dropdown
+//  DCS Filename  = must match the unit type string DCS expects
+//                  (this becomes the .lua filename)
+//
+//  To add an aircraft, just add a new line inside the array below.
+//  Order does not matter -- the dropdown sorts alphabetically.
+// ============================================================================
+
+const AIRCRAFT_DATA = [
+  ["A-10A",             "A-10A"],
+  ["A-10C",             "A-10C"],
+  ["A-10C II",          "A-10C_2"],
+  ["AH-64D",            "AH-64D_BLK_II"],
+  ["AJS-37 Viggen",     "AJS37"],
+  ["AV-8B N/A",         "AV8BNA"],
+  ["Bf 109 K-4",        "Bf-109K-4"],
+  ["C-101CC",           "C-101CC"],
+  ["C-101EB",           "C-101EB"],
+  ["Christen Eagle II", "Christen Eagle II"],
+  ["F-4E",              "F-4E-45MC"],
+  ["F-5E",              "F-5E-3"],
+  ["F-14A",             "F-14A-135-GR"],
+  ["F-14B",             "f-14b"],
+  ["F-15C",             "F-15C"],
+  ["F-15E",             "F-15ESE"],
+  ["F-16C",             "F-16C_50"],
+  ["F/A-18C",           "FA-18C_hornet"],
+  ["FW 190 A-8",        "FW-190A8"],
+  ["FW 190 D-9",        "FW-190D9"],
+  ["I-16",              "I-16"],
+  ["JF-17",             "JF-17"],
+  ["Ka-50",             "Ka-50"],
+  ["Ka-50 III",         "Ka-50_3"],
+  ["L-39C",             "L-39C"],
+  ["L-39ZA",            "L-39ZA"],
+  ["M-2000C",           "M-2000C"],
+  ["MB-339",            "MB-339A"],
+  ["Mi-8",              "Mi-8MT"],
+  ["Mi-24P",            "Mi-24P"],
+  ["MiG-15bis",         "MiG-15bis"],
+  ["MiG-19P",           "MiG-19P"],
+  ["MiG-21bis",         "MiG-21Bis"],
+  ["MiG-29A",           "MiG-29A"],
+  ["MiG-29S",           "MiG-29S"],
+  ["Mirage F1CE",       "Mirage-F1CE"],
+  ["Mirage F1EE",       "Mirage-F1EE"],
+  ["Mosquito FB Mk VI", "MosquitoFBMkVI"],
+  ["OH-58D",            "OH58D"],
+  ["P-47D",             "P-47D-30"],
+  ["P-51D",             "P-51D-30-NA"],
+  ["SA342 Gazelle",     "SA342M"],
+  ["Spitfire LF Mk IX", "SpitfireLFMkIX"],
+  ["Su-25",             "Su-25"],
+  ["Su-25T",            "Su-25T"],
+  ["Su-27",             "Su-27"],
+  ["Su-33",             "Su-33"],
+  ["TF-51D",            "TF-51D"],
+  ["UH-1H",             "UH-1H"],
+  ["Yak-52",            "Yak-52"],
+];
